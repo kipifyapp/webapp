f7df66dfb0b1fd775405f91309d8b5a9502369d7
Soham Borate <soham.borate1@gmail.com>
2023-10-09 22:36:19 +0530

    vercel blah blah

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ const app = express();
 
 app
 .set("view engine", "ejs")
-.set("views", `${__dirname}\\views`)
-.use(express.static(`${__dirname}\\public`))
+.set("views", `${__dirname}/views`)
+.use(express.static(`${__dirname}/public`))
 .use(cors())
 .use(cookieParser());
 
