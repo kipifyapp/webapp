2acd3f94e4dc64dc0cc3f74374f3d8d6e0c0e535
Soham Borate <soham.borate1@gmail.com>
2023-10-09 23:14:53 +0530

    forgot about this

--- index.js
+++ index.js
@@ -136,28 +136,4 @@ app.get("/callback", async function(req, res) {
     }
 });
 
-app.get("/refresh_token", function(req, res) {
-
-    // requesting access token from refresh token
-    var refresh_token = req.query.refresh_token;
-    var authOptions = {
-        url: "https://accounts.spotify.com/api/token",
-        headers: { "Authorization": "Basic " + (new Buffer(client_id + ":" + client_secret).toString("base64")) },
-        form: {
-            grant_type: "refresh_token",
-            refresh_token: refresh_token
-        },
-        json: true
-    };
-
-    request.post(authOptions, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var access_token = body.access_token;
-            res.send({
-                "access_token": access_token
-            });
-        }
-    });
-});
-
 module.exports = app;
