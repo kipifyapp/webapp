ffe0800fd242c612a61c8985e98611df2320610f
Soham Borate <soham.borate1@gmail.com>
2023-10-09 22:09:26 +0530

    v1.0.0

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Or if using `src` directory:
+    "./views/**/*.ejs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class",
+}
