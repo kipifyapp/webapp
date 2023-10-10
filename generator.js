24d2a960ddf54429d781b2fc2a8afae4bddd9dd9
Soham Borate <soham.borate1@gmail.com>
2023-10-10 20:04:55 +0530

    v1.1.0

--- generator.js
+++ generator.js
@@ -8,72 +8,22 @@ const {
     get_recommendations,
     get_artist,
     get_artists,
+    get_artist_top_tracks,
     get_top_items,
     get_all_top_items,
     get_user_profile,
+    check_user_saved_tracks,
     create_playlist,
     add_items_to_playlist
 } = require("./spotify-api.js");
 
-var top_artists;
-var top_tracks;
+const MAX = 50;
 
 function array_sum(arr) {
     let sum = arr.reduce(function (x, y) {
         return x + y;
     }, 0);
     return sum;
-}
-
-async function get_all_time_top(type, access_token) {
-    // const top_items_long = await get_top_items(type, access_token, { "time_range": "long_term" });
-    const top_items_med = await get_top_items(type, access_token);
-    const top_items_short = await get_top_items(type, access_token, { "time_range": "short_term" });
-    let items = [];
-
-    // top_items_long.items.map((item) => {
-    //     items.push(item);
-    // });
-    top_items_med.items.map((item) => {
-        items.push(item);
-    });
-    top_items_short.items.map((item) => {
-        items.push(item);
-    });
-
-    // remove duplicated
-    items = items.filter((item, index) => {
-        let searchIndex = items.findIndex((item2) => {
-            return item2.id === item.id;
-        });
-        return searchIndex === index;
-    });
-
-    return items;
-}
-
-async function get_5_artists() {
-    let items = top_artists;
-    let artists = [];
-    for (let i = 0; i < 5; i++) {
-        if (items.length === 0) break;
-        let random = Math.floor(Math.random() * items.length);
-        artists.push(items[random]);
-        items.splice(random, 1);
-    }
-    return artists;
-}
-
-async function get_5_tracks() {
-    let items = top_tracks;
-    let tracks = [];
-    for (let i = 0; i < 5; i++) {
-        if (items.length === 0) break;
-        let random = Math.floor(Math.random() * items.length);
-        tracks.push(items[random]);
-        items.splice(random, 1);
-    }
-    return tracks;
 }
 
 async function get_genres(artists) {
@@ -86,26 +36,30 @@ async function get_genres(artists) {
     return genres;
 }
 
-async function main(access_token) {
-    // top_artists = await get_all_time_top("artists", access_token);
-    // top_tracks = await get_all_time_top("tracks", access_token);
+async function main(access_token, playlist) {
+    if (!playlist) {
+        playlist = [];
+    }
+    if (playlist.length === MAX) {
+        return playlist;
+    }
 
-    // const artists = await get_5_artists();
-    // const tracks = await get_5_tracks();
-    const artists = (await get_top_items("artists", access_token, { "limit": 5 })).items;
-    const tracks = (await get_top_items("tracks", access_token, { "limit": 5 })).items;
-    const tracks_num = tracks.length;
+    const limit = MAX - playlist.length;
+
+    const artists = (await get_top_items("artists", access_token, { "time_range": "short_term", "limit": 5 })).items;
+    // const tracks = (await get_top_items("tracks", access_token, { "time_range": "short_term", "limit": 5 })).items;
+    // const tracks_num = tracks.length;
     // const features = (await get_tracks_audio_features(tracks.map((track) => track.id), access_token)).audio_features;
 
     const artists_seed = artists.map((artist) => artist.id);
-    const genres_seed = await get_genres(artists);
-    const tracks_seed = tracks.map((track) => track.id);
+    // const genres_seed = await get_genres(artists);
+    // const tracks_seed = tracks.map((track) => track.id);
 
     const options = {
-        "limit": 50,
+        "limit": limit,
         "seed_artists": artists_seed,
-        "seed_genres": genres_seed,
-        "seed_tracks": tracks_seed,
+        // "seed_genres": genres_seed,
+        // "seed_tracks": tracks_seed,
         // "target_acousticness": array_sum(features.map((track) => track.acousticness)) / tracks_num,
         // "target_danceability": array_sum(features.map((track) => track.danceability)) / tracks_num,
         // "target_duration_ms": array_sum(features.map((track) => track.duration_ms)) / tracks_num,
@@ -123,8 +77,27 @@ async function main(access_token) {
     }
 
     const recommendations = (await get_recommendations(access_token, options)).tracks;
+    const saved = await check_user_saved_tracks(recommendations.map((track) => track.id), access_token);
 
-    return recommendations;
+    recommendations.forEach((track, i) => {
+        if (!saved[i]) {
+            playlist.push(track);
+        }
+    });
+
+    // remove duplicates
+    playlist = playlist.filter((track, index) => {
+        let searchIndex = playlist.findIndex((track2) => {
+            return track2.id === track.id;
+        });
+        return searchIndex === index;
+    });
+
+    if (playlist.length !== MAX) {
+        playlist = await main(access_token, playlist);
+    }
+
+    return playlist;
 }
 
 module.exports = main;
