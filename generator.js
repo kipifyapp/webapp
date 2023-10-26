d424c8600818f96314b1525603e2127a8034760f
Soham Borate <soham.borate1@gmail.com>
2023-10-27 01:13:45 +0530

    Better algorithm

--- generator.js
+++ generator.js
@@ -1,5 +1,3 @@
-const querystring = require("querystring");
-
 const {
     get_track,
     get_tracks,
@@ -17,75 +15,39 @@ const {
     add_items_to_playlist
 } = require("./spotify-api.js");
 
+function shuffle_array(array) {
+    const shuffled = [];
+    const len = array.length;
+
+    for (let i = 0; i < len; i++) {
+        const index = Math.floor(Math.random() * array.length);
+        shuffled.push(array[index]);
+        array.splice(index, 1);
+    }
+
+    return shuffled;
+}
+
 const MAX = 50;
 
-function array_sum(arr) {
-    let sum = arr.reduce(function (x, y) {
-        return x + y;
-    }, 0);
-    return sum;
-}
-
-async function generate_from_profile(access_token, playlist) {
-    if (!playlist) {
-        playlist = [];
-    }
-    if (playlist.length === MAX) {
-        return playlist;
-    }
-
-    const limit = MAX - playlist.length;
-    const tracks = (await get_top_items("tracks", access_token, { "time_range": "short_term", "limit": 5 })).items;
-    const tracks_seed = tracks.map((track) => track.id);
-
+async function track(access_token, track_data, track_features) {
     const options = {
-        "limit": limit,
-        "seed_tracks": tracks_seed,
-    }
-
-    const recommendations = (await get_recommendations(access_token, options)).tracks;
-    const saved = await check_user_saved_tracks(recommendations.map((track) => track.id), access_token);
-
-    // remove saved
-    recommendations.forEach((track, i) => {
-        if (!saved[i]) {
-            playlist.push(track);
-        }
-    });
-
-    // remove duplicates
-    playlist = playlist.filter((track, index) => {
-        let searchIndex = playlist.findIndex((track2) => {
-            return track2.id === track.id;
-        });
-        return searchIndex === index;
-    });
-
-    if (playlist.length !== MAX) {
-        playlist = await generate_from_profile(access_token, playlist);
-    }
-
-    return playlist;
-}
-
-async function generate_from_profile_v2(access_token) {
-    const tracks = (await get_top_items("tracks", access_token, { "time_range": "short_term", "limit": 5 })).items;
-    const tracks_num = tracks.length;
-    const features = (await get_tracks_audio_features(tracks.map((track) => track.id), access_token)).audio_features;
-    const tracks_seed = tracks.map((track) => track.id);
-
-    const options = {
-        "limit": MAX,
-        "seed_tracks": tracks_seed,
-        "target_acousticness": array_sum(features.map((track) => track.acousticness)) / tracks_num,
-        "target_danceability": array_sum(features.map((track) => track.danceability)) / tracks_num,
-        "target_energy": array_sum(features.map((track) => track.energy)) / tracks_num,
-        "target_instrumentalness": array_sum(features.map((track) => track.instrumentalness)) / tracks_num,
-        "target_liveness": array_sum(features.map((track) => track.liveness)) / tracks_num,
-        "target_loudness": array_sum(features.map((track) => track.loudness)) / tracks_num,
-        "target_speechiness": array_sum(features.map((track) => track.speechiness)) / tracks_num,
-        "target_tempo": array_sum(features.map((track) => track.tempo)) / tracks_num,
-        "target_valence": array_sum(features.map((track) => track.valence)) / tracks_num
+        "limit": 99,
+        "seed_artists": track_data.artists[0].id,
+        "seed_tracks": [track_data.id],
+        "target_acousticness": track_features.acousticness,
+        "target_danceability": track_features.danceability,
+        "target_energy": track_features.energy,
+        "target_instrumentalness": track_features.instrumentalness,
+        "target_key": track_features.key,
+        "target_liveness": track_features.liveness,
+        "target_loudness": track_features.loudness,
+        "target_mode": track_features.mode,
+        "target_popularity": track_data.popularity,
+        "target_speechiness": track_features.speechiness,
+        "target_tempo": track_features.tempo,
+        "target_time_signature": track_features.time_signature,
+        "target_valence": track_features.valence
     }
 
     const recommendations = (await get_recommendations(access_token, options)).tracks;
@@ -93,72 +55,73 @@ async function generate_from_profile_v2(access_token) {
     return recommendations;
 }
 
-async function generate_from_tracks(access_token, tracks_seed, playlist) {
-    if (!playlist) {
-        playlist = [];
+async function generate_tracks(access_token, tracks_data) {
+    const tracks = [];
+    const suggested = new Map();
+
+    let tracks_features = (await get_tracks_audio_features(tracks_data.map((track) => track.id), access_token)).audio_features;
+
+    for (let i = 0; i < tracks_data.length; i++) {
+        let recommendations = await track(access_token, tracks_data[i], tracks_features[i]);
+        recommendations.map((track) => {
+            if (suggested.get(track.uri)) {
+                suggested.set(track.uri, suggested.get(track.uri) + 1);
+            } else {
+                suggested.set(track.uri, 1);
+            }
+        });
     }
-    if (playlist.length === MAX) {
-        return playlist;
-    }
 
-    const limit = MAX - playlist.length;
+    let top = 0;
+    const suggested2 = new Map();
 
-    const options = {
-        "limit": limit,
-        "seed_tracks": tracks_seed,
-    }
-
-    const recommendations = (await get_recommendations(access_token, options)).tracks;
-    const saved = await check_user_saved_tracks(recommendations.map((track) => track.id), access_token);
-
-    // remove saved
-    recommendations.forEach((track, i) => {
-        if (!saved[i]) {
-            playlist.push(track);
+    suggested.forEach((v, k) => {
+        if (suggested2.get(v)) {
+            let arr = suggested2.get(v);
+            arr[arr.length] = k;
+            suggested2.set(v, arr);
+        } else {
+            suggested2.set(v, [k]);
+        }
+        if (v > top) {
+            top = v;
         }
     });
 
-    // remove duplicates
-    playlist = playlist.filter((track, index) => {
-        let searchIndex = playlist.findIndex((track2) => {
-            return track2.id === track.id;
-        });
-        return searchIndex === index;
+    // randomise
+    suggested2.forEach((v, k) => {
+        suggested2.set(k, shuffle_array(v));
     });
 
-    if (playlist.length !== MAX) {
-        playlist = await generate_from_tracks(access_token, tracks_seed, playlist);
+    for (let i = top; i > 0; i--) {
+        suggested2.get(i).map((track) => {
+            if (tracks.length === MAX) {
+                return;
+            }
+            tracks.push(track);
+        });
     }
 
-    return playlist;
+    return tracks;
 }
 
-async function generate_from_tracks_v2(access_token, tracks_seed) {
-    const tracks_num = tracks_seed.length;
-    const features = (await get_tracks_audio_features(tracks_seed, access_token)).audio_features;
+async function generate_from_profile(access_token) {
+    const tracks_data = (await get_top_items("tracks", access_token, { "time_range": "short_term", "limit": 5 })).items;
 
-    const options = {
-        "limit": MAX,
-        "seed_tracks": tracks_seed,
-        "target_acousticness": array_sum(features.map((track) => track.acousticness)) / tracks_num,
-        "target_danceability": array_sum(features.map((track) => track.danceability)) / tracks_num,
-        "target_energy": array_sum(features.map((track) => track.energy)) / tracks_num,
-        "target_instrumentalness": array_sum(features.map((track) => track.instrumentalness)) / tracks_num,
-        "target_liveness": array_sum(features.map((track) => track.liveness)) / tracks_num,
-        "target_loudness": array_sum(features.map((track) => track.loudness)) / tracks_num,
-        "target_speechiness": array_sum(features.map((track) => track.speechiness)) / tracks_num,
-        "target_tempo": array_sum(features.map((track) => track.tempo)) / tracks_num,
-        "target_valence": array_sum(features.map((track) => track.valence)) / tracks_num
-    }
+    const recommendations = await generate_tracks(access_token, tracks_data);
 
-    const recommendations = (await get_recommendations(access_token, options)).tracks;
+    return recommendations;
+}
+
+async function generate_from_tracks(access_token, track_ids) {
+    const tracks_data = (await get_tracks(track_ids, access_token)).tracks;
+
+    const recommendations = await generate_tracks(access_token, tracks_data);
 
     return recommendations;
 }
 
 module.exports = {
-    generate_from_profile,
-    generate_from_profile_v2,
     generate_from_tracks,
-    generate_from_tracks_v2
+    generate_from_profile
 };
